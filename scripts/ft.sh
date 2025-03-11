fb682e9b30d90e210eb0258f32ed4effcf608d07
Hewei Gao <ghw1515489084@gmail.com>
2025-03-11 22:43:53 +0100

    env: change folder architecture

--- scripts/ft.sh
+++ scripts/ft.sh
@@ -19,7 +19,7 @@
 
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=0 python scripts/finetune.py \
+CUDA_VISIBLE_DEVICES=0 python musicbert_hf/scripts/finetune.py \
     data_dir=/zhome/stud/zhangya/yyang/datasets/rnbert_h5 \
     output_dir_base=/zhome/stud/zhangya/yyang/output/musicbert_hf/finetune \
     targets="[key_pc_mode, quality, harmony_onset, inversion, primary_alteration_primary_degree_secondary_alteration_secondary_degree]" \
@@ -33,4 +33,4 @@ CUDA_VISIBLE_DEVICES=0 python scripts/finetune.py \
     freeze_layers=9 \
     max_steps=50000 \
     warmup_steps=2500 \
-    report_to=wandb
+    # report_to=wandb
