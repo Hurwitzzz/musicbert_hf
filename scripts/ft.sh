742d3f38f26f36478f2eba40520e7c9e0a69399b
Hewei Gao <ghw1515489084@gmail.com>
2025-04-02 16:57:36 +0200

    env: pwd changed, update paths

--- scripts/ft.sh
+++ scripts/ft.sh
@@ -4,7 +4,7 @@
 #     --data_dir /zhome/stud/zhangya/yyang/datasets_debug/rnbert_h5\
 #     --output_dir_base /zhome/stud/zhangya/yyang/output/musicbert_hf/finetune \
 #     --targets key_pc_mode quality \
-#     --checkpoint_path /home/stud/zhangya/yyang/BERT/ckpts/pretrained/checkpoint_last_musicbert_base.pt \
+#     --checkpoint_path /home/stud/zhangya/yyang/.BERT/ckpts/pretrained/checkpoint_last_musicbert_base.pt \
 #     --eval_steps 2500 \
 #     --save_steps 2500 \
 #     --learning_rate 2.5e-5 \
@@ -23,7 +23,7 @@ CUDA_VISIBLE_DEVICES=0 python musicbert_hf/scripts/finetune.py \
     data_dir=/zhome/stud/zhangya/yyang/datasets/rnbert_h5 \
     output_dir_base=/zhome/stud/zhangya/yyang/output/musicbert_hf/finetune \
     targets="[key_pc_mode, quality, harmony_onset, inversion, primary_alteration_primary_degree_secondary_alteration_secondary_degree]" \
-    checkpoint_path=/home/stud/zhangya/yyang/BERT/ckpts/pretrained/checkpoint_last_musicbert_base.pt \
+    checkpoint_path=/home/stud/zhangya/yyang/.BERT/ckpts/pretrained/checkpoint_last_musicbert_base.pt \
     eval_steps=2500 \
     save_steps=2500 \
     learning_rate=2.5e-5 \
